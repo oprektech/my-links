86bb9d67258c936ebfa7b68f20aeced66e4f9ba6
oprektech <oprek.tech@gmail.com>
2026-01-11 23:16:12 +0700

    Update db.php

--- db.php
+++ db.php
@@ -3,9 +3,20 @@ $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $db   = getenv('DB_NAME');
+$port = (int)getenv('DB_PORT'); // Pastikan dibaca sebagai angka
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+// Coba koneksi dengan timeout 5 detik
+$conn = mysqli_init();
+if (!$conn) { die("mysqli_init failed"); }
+
+mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+
+// Melakukan koneksi
+$konek = @mysqli_real_connect($conn, $host, $user, $pass, $db, $port);
+
+if (!$konek) {
+    die("Gagal Konek Database: " . mysqli_connect_error());
+}
+
 mysqli_set_charset($conn, "utf8mb4");
-
-if (!$conn) { die("Koneksi gagal"); }
 ?>
