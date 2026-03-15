e78e751c592c15c027bacae523a68fa0ac4414e9
oprektech <oprek.tech@gmail.com>
2026-03-15 21:56:12 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,11 @@
 <?php
 
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+$host = "sql105.infinityfree.com";
+$user = "if0_41397338";
+$pass = "Yoga2309071945";
+$db   = "if0_41397338_mylinks";
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$conn = mysqli_connect($host,$user,$pass,$db);
 
 if(!$conn){
     die("Database connection failed");
