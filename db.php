5e4a7a3e8ddf0b9a8e4e66b3d10eb3606cf31a77
oprektech <oprek.tech@gmail.com>
2026-03-15 21:16:11 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,22 +1,17 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-$port = (int)getenv('DB_PORT'); // Pastikan dibaca sebagai angka
 
-// Coba koneksi dengan timeout 5 detik
-$conn = mysqli_init();
-if (!$conn) { die("mysqli_init failed"); }
+$host = "https://oaecibokcofhmhzdojak.supabase.co";
+$port = "5432";
+$db   = "postgres";
+$user = "postgres";
+$pass = "Yoga2309071945";
 
-mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+$conn = pg_connect(
+  "host=$host port=$port dbname=$db user=$user password=$pass"
+);
 
-// Melakukan koneksi
-$konek = @mysqli_real_connect($conn, $host, $user, $pass, $db, $port);
-
-if (!$konek) {
-    die("Gagal Konek Database: " . mysqli_connect_error());
+if(!$conn){
+    die("Database connection failed");
 }
 
-mysqli_set_charset($conn, "utf8mb4");
 ?>
